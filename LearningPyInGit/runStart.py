739a0141c45a1d0deb030d28ff1f4c562b041933
jean-thierry <j-tyry@users.noreply.github.com>
2014-06-15 07:38:45 -0400

    added some classes. still in learning process. This is so cool!!! :)

--- LearningPyInGit/runStart.py
+++ LearningPyInGit/runStart.py
@@ -3,18 +3,35 @@ author jean-thierry roy
 date 2014-06-14
 '''
 
-from utilities.utils import hello_class
+from utilities.utils import *
 
 #hello()
 
 hello_class.i
 
+# assign an imported class object to a
+# then, of course, print a
 a = hello_class.age
-print a
+print 'a = hello_class.age : ', a
 
+# assign __doc__ of class to b
+# then print b
 b = hello_class.__doc__
-print b
+print 'b = hello_class.__doc__ : ', b
 
+# Instance class to c
+# then use instance to do some mods
+# print the original class and the moded class instance
 c = hello_class()
 c.i += 4
-print hello_class.i, c.i 
+print 'hello_class.i : ', hello_class.i, '\rc.i : ', c.i
+
+# using the instanced class method (the def).
+d = c.hello_w()
+print 'd : ', d
+
+# using a class with an __init__ method
+# assign class argument
+# print the class argument
+e = init_class('Hello to world')
+print 'e.data : ', e.data
--- LearningPyInGit/utilities/utils.py
+++ LearningPyInGit/utilities/utils.py
@@ -1,4 +1,3 @@
-
 '''
 author jean-thierry roy
 date 2014-06-14
@@ -9,9 +8,14 @@ def hello():
     print 'hello world'
     
 class hello_class:
-    """ A very simple class in py """
+    """A very simple class in py."""
     i = 123456
     name = 'john'
     age = 46
     def hello_w(self):
         return 'Hello world'
+
+class init_class:
+    """A class with an __init__"""
+    def __init__(self, string):
+        self.data = string
