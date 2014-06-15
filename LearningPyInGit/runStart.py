8c4693766687e2ce585a633c0c70cbd21125c80c
jean-thierry <j-tyry@users.noreply.github.com>
2014-06-15 06:50:57 -0400

    added a simple class. keep on trying...

--- LearningPyInGit/runStart.py
+++ LearningPyInGit/runStart.py
@@ -3,6 +3,18 @@ author jean-thierry roy
 date 2014-06-14
 '''
 
-from utilities.utils import *
+from utilities.utils import hello_class
 
-hello()
+#hello()
+
+hello_class.i
+
+a = hello_class.age
+print a
+
+b = hello_class.__doc__
+print b
+
+c = hello_class()
+c.i += 4
+print hello_class.i, c.i 
--- LearningPyInGit/utilities/utils.py
+++ LearningPyInGit/utilities/utils.py
@@ -7,3 +7,11 @@ date 2014-06-14
 
 def hello():
     print 'hello world'
+    
+class hello_class:
+    """ A very simple class in py """
+    i = 123456
+    name = 'john'
+    age = 46
+    def hello_w(self):
+        return 'Hello world'
